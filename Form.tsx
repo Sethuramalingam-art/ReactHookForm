f1cc087e5761d6bc7fa7f8ee593e67921314b5c5
Sethuramalingam Duraisamy <59419619+Sethuramalingam-art@users.noreply.github.com>
2023-06-23 16:45:27 +0530

    Add files via upload

---
+++ Form.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+
+const Form = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data: any) => {
+    console.log(data);
+  };
+  return (
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="form-control">
+          <label>Email</label>
+          <input
+            type="text"
+            {...register("email", {
+              required: true,
+              minLength: 6,
+            })}
+          />
+          {errors.email && errors.email.type === "required" && (
+            <p className="errorMsg">Email is required.</p>
+          )}
+          {errors.email && errors.email.type === "pattern" && (
+            <p className="errorMsg">Email is not valid.</p>
+          )}
+        </div>
+        <div className="form-control">
+          <label>Password</label>
+          <input
+            type="password"
+            {...register("password", {
+              required: true,
+              minLength: 6,
+            })}
+          />
+          {errors.password && errors.password.type === "required" && (
+            <p className="errorMsg">Password is required.</p>
+          )}
+          {errors.password && errors.password.type === "minLength" && (
+            <p className="errorMsg">
+              Password should be at-least 6 characters.
+            </p>
+          )}
+        </div>
+        <div className="form-control">
+          <button type="submit">Login</button>
+        </div>
+      </form>
+    </>
+  );
+};
+
+export default Form;
